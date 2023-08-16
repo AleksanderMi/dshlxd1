bdbd21e2d44526766b5c511a87a35ac0337ae7f7
AleksanderMi <amilde@insolutions.pl>
2023-08-16 15:55:51 +0200

    navbar changes with some other minor improvements

--- dshlxd1/public/js/status_change.js
+++ dshlxd1/public/js/status_change.js
@@ -1,4 +1,9 @@
 import $ from 'jquery';
+const oko1 = document.getElementById('oko1');
+const oko2 = document.getElementById('oko2');
+const usta = document.getElementById('usta');
+oko1.style.fill = "url(#linearGradient-1)";
+let bool =1;
 $(document).ready(function() {
     let $container = $('.tutaj');
     $container.find('i').on('click', function(e) {
@@ -18,4 +23,21 @@ $(document).ready(function() {
             }
         });
     });
+   oko1.addEventListener('click', function(e){
+        e.preventDefault();
+        console.log("klik");
+        if (bool == 1) {
+            oko1.style.fill = "url(#linearGradient-2)";
+            oko2.style.fill = "url(#linearGradient-2)";
+            usta.style.cy = 290;
+            bool = 0;
+        }
+        else{
+            oko1.style.fill = "url(#linearGradient-1)";
+            oko2.style.fill = "url(#linearGradient-1)";
+            usta.style.cy = -150;
+            bool = 1;
+        }
+
+   });
 });
